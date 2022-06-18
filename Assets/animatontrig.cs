366ab0e7dd46915a3ad9a59ac0ae37d83df06d2e
kwongchishing <106185163+kwongchishing@users.noreply.github.com>
2022-06-19 00:49:41 +0800

    pet

--- Assets/animatontrig.cs
+++ Assets/animatontrig.cs
@@ -1,7 +1,13 @@
 using UnityEngine;public class animatontrig:MonoBehaviour{    
     Animator anim;
+    public AudioSource wait;
+    public Transform P1,P2;
     void Start(){anim=GetComponent<Animator>();}
     void Update(){
-        anim.SetTrigger("a1");
+        if(Vector3.Distance(P1.transform.position,transform.position)<20f||Vector3.Distance(P2.transform.position,transform.position)<20f){anim.SetTrigger("a1");}
+        else{anim.ResetTrigger("a1");}
+    }
+    public void idlesound(){
+        wait.Play();
     }
 }
