af4d25494c2f7382f128b3bf3ad3286e9b0a6e46
kwongchishing <106185163+kwongchishing@users.noreply.github.com>
2022-06-24 23:51:38 +0800

    weaponhouse

--- Assets/transferWeaponHouse.cs
+++ Assets/transferWeaponHouse.cs
@@ -8,12 +8,12 @@ using UnityEngine;public class transferWeaponHouse:MonoBehaviour{
         distance=Vector3.Distance(p1.transform.position,p2.transform.position);
         if(Input.GetKeyDown(KeyCode.Return)||Input.GetKeyDown(KeyCode.E)){
             opendoorsound.Play();
-            Player.transform.position=new Vector3(-2634.84229f,-93.1f,-6007.70264f);
+            Player.transform.position=new Vector3(-207.95802307128907f, -138.77f, 122.84185791015625f);
             save2.isinshop=true;
             minimap.SetActive(false);
             if(distance<=8f&&save2.isjoined==true){
                 save2.isinshop=true;
-                Player2.transform.position=new Vector3(-2633.558837890625f,-93.1f, -6009.97802734375f);
+                Player2.transform.position=new Vector3(-211.01437377929688f, -138.77f, 122.89533233642578f);
             }
         }
     }
---
+++ Assets/transferWeaponhousesister.cs
@@ -0,0 +1,20 @@
+using UnityEngine;public class transferWeaponhousesister:MonoBehaviour{
+    public GameObject Player2;
+    public float distance;
+    public Transform p1,p2;
+    public save2 save2;
+    public AudioSource opendoorsound;
+    public GameObject Player1;
+    void Update(){
+        distance=Vector3.Distance(p1.transform.position,p2.transform.position);
+        if(Input.GetKeyDown(KeyCode.KeypadEnter)){
+            opendoorsound.Play();
+            Player2.transform.position=new Vector3(-211.01437377929688f, -138.77f, 122.89533233642578f);
+            save2.isinshop=true;
+            if(distance<=8f){
+                Player1.transform.position=new Vector3(-207.95802307128907f, -138.77f, 122.84185791015625f);
+                save2.isinshop=true;
+            }
+        }
+    }
+}
