7e350feaa7efb04fd034d1a36adec70726a39a05
kwongchishing <106185163+kwongchishing@users.noreply.github.com>
2022-06-26 21:38:12 +0800

    distanceshowupgradepanel update

--- Assets/distanceshowupgrade.cs
+++ Assets/distanceshowupgrade.cs
@@ -1,7 +1,7 @@
 using UnityEngine;public class distanceshowupgrade:MonoBehaviour{
     public GameObject Player,Player2,mainupgradeCanvas;
     void Update(){
-        if(Vector3.Distance(Player.transform.position,transform.position)<6f||Vector3.Distance(Player2.transform.position,transform.position)<6f){
+        if(Vector3.Distance(Player.transform.position,transform.position)<6.2f||Vector3.Distance(Player2.transform.position,transform.position)<6.2f){
             mainupgradeCanvas.SetActive(true);
         }
         else{
